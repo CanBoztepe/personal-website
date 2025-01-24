481e18aee5ce22c0432854825b715dd7022cff5c
Can Boztepe <canboztepe@gmail.com>
2025-01-24 15:51:27 -0500

    added links to personalInfo store

--- src/stores/PersonalInfo.js
+++ src/stores/PersonalInfo.js
@@ -8,6 +8,12 @@ export let usePersonalInfoStore = defineStore('personalInfo', {
       slogan:
         "I'm a self-taught developer who strives to write readable, maintainable and scalable code.",
       skills: ['VueJS', 'JavaScript', 'HTML', 'CSS', 'Git'],
+      links: {
+        github: 'https://github.com/CanBoztepe',
+        linkedin: 'https://www.linkedin.com/in/can-boztepe/',
+        codepen: 'https://codepen.io/CanBoztepe',
+        leetcode: 'https://leetcode.com/u/can-boztepe/',
+      },
     }
   },
 })
