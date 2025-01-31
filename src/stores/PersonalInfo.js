6c0b8d36a2990693c34f8e28218264f8c51a5ea8
Can Boztepe <canboztepe@gmail.com>
2025-01-30 22:34:38 -0500

    update skills

--- src/stores/PersonalInfo.js
+++ src/stores/PersonalInfo.js
@@ -7,7 +7,7 @@ export let usePersonalInfoStore = defineStore('personalInfo', {
       title: 'Frontend Developer',
       slogan:
         "I'm a self-taught developer who strives to write readable, maintainable and scalable code.",
-      skills: ['VueJS', 'JavaScript', 'HTML', 'CSS', 'Git'],
+      skills: ['VueJS', 'JavaScript', 'TypeScript', 'HTML', 'CSS', 'Git'],
       links: {
         github: 'https://github.com/CanBoztepe',
         linkedin: 'https://www.linkedin.com/in/can-boztepe/',
