33d7bf61ee33d885a02694dd267789fec20360b7
Can Boztepe <canboztepe@gmail.com>
2025-01-30 21:52:44 -0500

    add description to store

--- src/stores/CoolProjects.js
+++ src/stores/CoolProjects.js
@@ -10,6 +10,7 @@ export let useCoolProjectsStore = defineStore('coolProjects', {
           id: 1,
           name: 'canboztepe.com',
           imgSrc: PersonalWebsiteImg,
+          description: 'My portfolio site where I showcase Vue proficiency',
           demoLink: '',
           githubLink: 'https://github.com/CanBoztepe/personal-website',
           codepenLink: '',
