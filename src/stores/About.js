d32891d120b0f3fefcfb140182e0a0af2ee684b6
Can Boztepe <canboztepe@gmail.com>
2025-02-02 20:55:12 -0500

    update store

--- src/stores/About.js
+++ src/stores/About.js
@@ -9,17 +9,15 @@ export const useAboutStore = defineStore('about', {
       under pressure and collaborate effectively in high-paced, team-oriented environments.
       <br /><br />
       After COVID I transitioned into web development, learning JavaScript, HTML, and CSS, and landed an internship
-      at a startup where I was hired full-time as a Junior Front-End Developer. During that time, I
-      worked closely with a senior full-stack developer to build production-ready Vue components,
-      further sharpening my problem-solving and communication skills.
+      at a startup where I was hired full-time as a Junior Front-End Developer.
       <br /><br />
-      Driven by best practices, responsiveness, and accessibility, I thrive in fast-paced startup
+      Driven by best practices, I thrive in fast-paced startup
       settings where I can combine creativity and technical expertise to solve problems. I’m
       proficient with <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer" class="about-link">Vue</a>
       and always eager to learn new frameworks, tools, and languages.
       <br /><br />
       Outside of coding, I enjoy going for walks, hitting the gym, and making gaming videos—activities
-      that help me stay balanced, curious, and solution-oriented. I look forward to contributing my
+      that help me stay balanced and curious. I look forward to contributing my
       adaptability, teamwork, and passion for continuous growth to a forward-thinking development
       team.
     `,
